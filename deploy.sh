c0fbc6a9fda430907fb829f835ce59c8ec8eb3a5
eldrid01 <matthew.d.eldridge@gmail.com>
2017-09-29 19:44:18 +0100

    Added volume mapping for log directory.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-mkdir -p /opt/shiny/g4hunter/logs
-chmod ugo+rwx /opt/shiny/g4hunter/logs
-docker run -u shiny --rm -p 80:3838 crukci-bioinformatics/g4hunter:latest &
+mkdir -p logs
+chmod ugo+rwx logs
+docker run -u shiny -d --rm -p 8080:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/g4hunter:latest
 sleep 2
 docker ps -a
