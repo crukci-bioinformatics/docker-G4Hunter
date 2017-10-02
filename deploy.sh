bdb79a8830fe78e76e9df36137971d003423dc88
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-02 11:59:30 +0100

    Now runs as daemon process.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 mkdir -p logs
 chmod ugo+rwx logs
-docker run -u shiny --rm -p 8080:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/g4hunter:latest &
-sleep 2
-docker ps -a
+docker run -u shiny -d -p 8080:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/g4hunter
