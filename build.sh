7415df67200b4243c3bd0d709c9d84bec577c8e2
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-13 07:21:41 +0100

    Removed superfluous commented lines

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
 rm -rf G4Hunter
-
 git clone https://github.com/LacroixLaurent/G4Hunter.git G4Hunter
-
-# build docker image
 docker build --tag="crukci-bioinformatics/g4hunter" .
-
-# or build docker image from scratch without using cache
-#docker build --tag="crukci-bioinformatics/g4hunter" --no-cache .
-
-# remove dangling/untagged images
-#docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-
