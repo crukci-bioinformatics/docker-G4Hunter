2fc9b7b99cd9c2a8293ec1a9e2caceec952ba893
eldrid01 <eldrid01@nm156s016766.cri.camres.org>
2017-10-14 17:21:14 +0100

    Reformatting for readability

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir -p logs
 chmod ugo+rwx logs
-docker run -u shiny -d -p 8080:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/g4hunter
+docker run -u shiny -d --rm -p 80:3838 \
+  -v ${PWD}/logs:/var/log/shiny-server \
+  crukci-bioinformatics/g4hunter
